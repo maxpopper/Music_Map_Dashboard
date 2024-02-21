fe351ba5bd4832b7109e80ac781d9f8bd6936bfb
Max Popper <maxpopper@wustl.edu>
2024-02-21 15:29:48 -0500

    starter files

---
+++ logic.js
@@ -0,0 +1,14 @@
+// Creating our initial map object:
+// We set the longitude, latitude, and starting zoom level.
+// This gets inserted into the div with an id of "map".
+let myMap = L.map("map", {
+    center: [10, 10],
+    zoom: 3
+  });
+  
+  // Adding a tile layer (the background map image) to our map:
+  // We use the addTo() method to add objects to our map.
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(myMap);
+  
