741d89c52ded851ed89566fe5013c6385c34d45c
davidcastanoe <david.castanoe@yahoo.com>
2024-02-27 13:22:22 -0500

    Update PY, create Data, Update HTML

--- logic.js
+++ logic.js
@@ -1,14 +1,13 @@
 // Creating our initial map object:
 // We set the longitude, latitude, and starting zoom level.
 // This gets inserted into the div with an id of "map".
-let myMap = L.map("map", {
-    center: [10, 10],
-    zoom: 3
-  });
+console.log(latin_america_region)
+
+let myMap = L.map("map", {center: [10, 13],zoom: 2});
   
   // Adding a tile layer (the background map image) to our map:
   // We use the addTo() method to add objects to our map.
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(myMap);
+}).addTo(myMap);
   
